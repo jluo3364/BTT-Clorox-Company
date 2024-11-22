1992c534bf5e4943db516a850f4e455ec5ec72ed
jluo3364 <77076660+jluo3364@users.noreply.github.com>
2024-11-22 01:00:30 -0500

    clean generated topic label and function to get subcategories of desired sizes

--- TopicModel/LSA.py
+++ TopicModel/LSA.py
@@ -72,6 +72,7 @@ class LSA(TopicModel):
                 top_word_indices = np.argsort(topic_vectors[i])[-15:]  # get the indices of top 15 words
                 topic_words = [terms[idx] for idx in top_word_indices]
                 topic_label = generate_topic_label(topic_words, rating)
+                topic_label = self.clean_topic_label(topic_label)
                 if verbose == 2:
                     print(f"Topic {i}: {topic_label}\n\t{topic_words}")
                 topic_words_labels[i] = [topic_words, topic_label]
