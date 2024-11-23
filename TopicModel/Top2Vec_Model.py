f683151ea932650de53e5bdf9aebc117785e1bf3
saloni-jain-code <sjain102015@gmail.com>
2024-11-22 23:00:22 -0500

    fixed similarity score bugs in Top2Vec class

--- TopicModel/Top2Vec_Model.py
+++ TopicModel/Top2Vec_Model.py
@@ -20,10 +20,14 @@ class Top2Vec_Model(TopicModel):
             self.train_model_subcategory(subcategory) 
         return self.df
     
-    def train_model_subcategory(self, subcategory):
+    def train_model_subcategory(self, subcategory, verbose=True):
         subset_df = self.df[self.df['subcategory'] == subcategory]
+        self.df['top2vec_topic_number'] = -1
+        self.df['top2vec_topic_words'] = ''
+        self.df['top2vec_topic_label'] = ''
+        self.df['top2vec_similarity_score'] = -1
         subset_df['top2vec_topic_number'] = -1
-        subset_df['top2vec_top_15_words'] = ''
+        subset_df['top2vec_topic_words'] = ''
         subset_df['top2vec_topic_label'] = ''
         subset_df['top2vec_similarity_score'] = -1
         
@@ -35,14 +39,16 @@ class Top2Vec_Model(TopicModel):
         desired_num_topics = []
         for i in range(1, 6):
             desired_num_topics.append(calculate_num_topics_star_rating(num_topics_to_generate, i))
-
+        
         # Perform Hierarchical Topic Reduction if necessary
         top2vec_models = []
         topic_reduction = [False]*5
         for i in range(len(star_rating_dataframes)):
             star_rating_dataframe = star_rating_dataframes[i]
-            top2vec_model = Top2Vec(documents=list(star_rating_dataframe['review_text']), speed='deep-learn', embedding_model='doc2vec', workers=20, ngram_vocab=True)
-            top2vec_model.save("top2vec_saved_models/top2vec_" + str(star_rating_dataframe['star_rating'].iloc[0]) + "_star")
+            if verbose:
+                print(f"Creating Top2Vec model with {desired_num_topics[i]} topics for {star_rating_dataframe.shape[0]} reviews (star rating {i+1}) in subcategory {subcategory}")
+            top2vec_model = Top2Vec(documents=list(star_rating_dataframe['review_text']), embedding_model='all-MiniLM-L6-v2', workers=20, ngram_vocab=True)
+            # top2vec_model.save("top2vec_saved_models/top2vec_" + str(star_rating_dataframe['star_rating'].iloc[0]) + "_star")
             topics_generated = top2vec_model.get_num_topics()
             if topics_generated > desired_num_topics[i]:
                 # do topic reduction 
@@ -59,20 +65,26 @@ class Top2Vec_Model(TopicModel):
                 if refined_topics.get(i+1) is None:
                     refined_topics[i+1] = []
                 refined_topics[i+1].append(generate_topic_label(top_15_topic_words, i+1)) # i+1 because ratings are 1-indexed
-
+        
         # Assign topics to each review and add relevant columns to df
         for i in range(len(top2vec_models)):
+            print("STAR RATING ", i+1)
+            print("COLUMNS", star_rating_dataframes[i].columns)
             topic_nums, topic_score, topic_words, word_scores = top2vec_models[i].get_documents_topics(doc_ids=top2vec_models[i].document_ids, reduced=topic_reduction[i])
             star_rating_dataframes[i]['top2vec_topic_number'] = topic_nums
             star_rating_dataframes[i]['top2vec_topic_label'] = star_rating_dataframes[i]['top2vec_topic_number'].apply(lambda x: refined_topics[i+1][x])
-            star_rating_dataframes[i]['top2vec_top_15_words'] = star_rating_dataframes[i]['top2vec_topic_number'].apply(lambda x: topic_words[x][:15])
-            star_rating_dataframes[i].reset_index(drop=True, inplace=True)
+            star_rating_dataframes[i]['top2vec_topic_words'] = star_rating_dataframes[i]['top2vec_topic_number'].apply(lambda x: str(topic_words[x][:15]))
+            # star_rating_dataframes[i].reset_index(drop=True, inplace=True)
 
         # Combine dataframes
         new_df = pd.concat(star_rating_dataframes)
-
+        # replace rows in self.df with the rows in new_df
+        self.df.loc[new_df.index] = new_df
         # Calculate similarity scores
         self.calculate_similarity_score(subcategory, True)
         self.calculate_similarity_score(subcategory, False)
 
-        return new_df
+        for i in range(len(top2vec_models)):
+            self.models[i+1] = top2vec_models[i]
+        
+        return self.df
